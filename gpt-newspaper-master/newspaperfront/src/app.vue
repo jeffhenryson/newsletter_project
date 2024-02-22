0344ce5fcfd53db61e555f9047700d80ee7a6f76
jeffhenryson <jeffhunbruey@gmail.com>
2024-02-21 23:48:18 -0300

    Mudanças

---
+++ gpt-newspaper-master/newspaperfront/src/app.vue
@@ -0,0 +1,28 @@
+// App.vue
+<template>
+  <div class="container">
+    <HeaderComponent />
+    <TopicSelectionComponent />
+    <LayoutSelectionComponent />
+    <NewspaperComponent />
+    <LoadingComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import TopicSelectionComponent from './components/TopicSelectionComponent.vue';
+import LayoutSelectionComponent from './components/LayoutSelectionComponent.vue';
+import NewspaperComponent from './components/NewspaperComponent.vue';
+import LoadingComponent from './components/LoadingComponent.vue';
+
+export default {
+  components: {
+    HeaderComponent,
+    TopicSelectionComponent,
+    LayoutSelectionComponent,
+    NewspaperComponent,
+    LoadingComponent
+  }
+};
+</script>
---
+++ gpt-newspaper-master/newspaperfront/src/components/Header/HeaderComponent.vue
@@ -0,0 +1,20 @@
+// HeaderComponent.vue
+<template>
+  <header>
+    <div class="header-content">
+      <h1>Gpt Newspaper</h1>
+    </div>
+    <p class="powered-by">Powered by <a href="https://tavily.com" target="_blank">Tavily</a></p>
+    <p class="topics-interest">Type topics of interest and get your personalized newspaper</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent'
+};
+</script>
+
+<style scoped>
+/* Seus estilos para HeaderComponent aqui ou importe de assets */
+</style>
